73efdfffac055c593ca563c555dab1c782ae0086
jasonmcaffee <jasonlmcaffee@gmail.com>
2012-07-15 16:46:46 -0400

    nav bar working well on all modern browsers.

--- src/js/lib/views/HomeView.js
+++ src/js/lib/views/HomeView.js
@@ -15,8 +15,11 @@ define([
             log('HomeView.render called.');
             var self = this;
             $(function(){
+                log('HomeView.render executing now that dom is ready...');
                 //render the template function to the dom
-                self.$el.html(homePageTemplateFunction());
+                //var newHtml = homePageTemplateFunction();
+                //log('newHtml is : ' + newHtml);
+                self.$el.html(homePageTemplateFunction()); //todo: this doesn't work in opera
             });
         }
     });
