85fd9fbd80d0ee53d68cf63a621289ef82cddd32
jasonmcaffee <jasonlmcaffee@gmail.com>
2012-08-05 15:36:36 -0400

    responsive master detail list - continued improving. good point to demo limits of responsive design. detail list shows up underneath masterlist, but it'd be nice to have detail list be a separate page/view for small devices.

---
+++ src/js/lib/models/ResponsiveDemoModel.js
@@ -0,0 +1,7 @@
+
+define([
+    'core/util/log',
+    ''
+], function(){
+
+});
--- src/js/lib/views/ResponsiveDemoView.js
+++ src/js/lib/views/ResponsiveDemoView.js
@@ -2,13 +2,19 @@ define([
     'core/util/log',
     'backbone',
     'jquery',
-    'compiled-templates/demos/responsiveDemoPageTemplate'
-], function(log, Backbone, $, responsiveDemoPageTemplate){
+    'compiled-templates/demos/responsiveDemoPageTemplate',
+    'lib/widgets/ResponsiveMasterDetailList'
+], function(log, Backbone, $, responsiveDemoPageTemplate, ResponsiveMasterDetailListWidget){
 
     var ResponsiveDemoView = Backbone.View.extend({
         el:'#pages',
+        events:{
+            "click #masterList li" : "masterListItemClick"
+        },
         initialize : function(){
             log('ResponsiveDemoView.initialize called.');
+
+            this.responsiveMasterDetailListWidget = new ResponsiveMasterDetailListWidget({el:''});
 
         },
         render: function(){
@@ -17,7 +23,13 @@ define([
             $(function(){
                 //render the template function to the dom
                 self.$el.html(responsiveDemoPageTemplate());
+
+                //wire up ResponsiveMasterDetailListWidget
             });
+        },
+        masterListItemClick : function(e){
+            var $listItem = $(e.target);
+            log('masterListItemClick : {0}', $listItem.html());
         }
     });
 
